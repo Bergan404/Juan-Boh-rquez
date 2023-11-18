211805f8600e4f1c3557d7997ef6a81b3ad278dc
Bergan Oudshoorn <bergan404@gmail.com>
2023-11-18 16:24:28 -0500

    using tresjs for 3d assets

--- README.md
+++ README.md
@@ -52,3 +52,9 @@ All commands are run from the root of the project, from a terminal:
 ## 👀 Want to learn more?
 
 Feel free to check [our documentation](https://docs.astro.build) or jump into our [Discord server](https://astro.build/chat).
+
+https://astro-bootstrap.github.io/getting-started/
+
+npm install three @tresjs/core @tresjs/cientos
+
+npm install @types/three -D
---
+++ src/components/TheExperience.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { Stars, OrbitControls } from '@tresjs/cientos';
+import { TresCanvas } from '@tresjs/core';
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three/src/constants.js';
+
+const gl = {
+    clearColor: "#181c3E",
+    shadows: true,
+    alpha: false,
+    shadowMapType: BasicShadowMap,
+    outputColorSpace: SRGBColorSpace,
+    toneMapping: NoToneMapping,
+}
+
+</script>
+<template>
+    <div class="tres-container">
+        <TresCanvas v-bind="gl">
+            <!-- Camera Postion in the X Y Z -->
+            <TresPerspectiveCamera :position="[0, 0, 5]" />
+            <TresMesh>
+                <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+                <TresMeshBasicMaterial color="white" />
+            </TresMesh>
+            <Stars />
+            <OrbitControls />
+        </TresCanvas>
+    </div>
+</template>
+
+<style>
+.tres-container {
+    width: 100%;
+    height: 100vh;
+}
+
+canvas {
+    width: 100%;
+    height: 100%;
+}
+</style>
