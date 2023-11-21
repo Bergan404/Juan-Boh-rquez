e2702a25ac583a784114c86f52757d296d6a34e5
Bergan Oudshoorn <bergan404@gmail.com>
2023-11-21 11:40:30 -0500

    got the carousel working and added another one some formatting had to be done but its good now

--- src/assets/scripts/carousel.js
+++ src/assets/scripts/carousel.js
@@ -1,12 +1,16 @@
 
 // Get all radio buttons
-const radioButtons = document.querySelectorAll('.radio-button');
+const radioButtonsAlbum = document.querySelectorAll('.radio-button-album');
+const radioButtonsApparel = document.querySelectorAll('.radio-button-apparel');
 
-radioButtons.forEach(button => {
-    button.addEventListener('click', moveCheckedAttribute);
+radioButtonsAlbum.forEach(button => {
+    button.addEventListener('click', moveCheckedAttributeAlbum);
+});
+radioButtonsApparel.forEach(button => {
+    button.addEventListener('click', moveCheckedAttributeApparel);
 });
 
-function moveCheckedAttribute(event) {
+function moveCheckedAttributeAlbum(event) {
     // Identify the clicked radio button
     const clickedButton = this;
     console.log(clickedButton, "this what we pressed right")
@@ -18,7 +22,32 @@ function moveCheckedAttribute(event) {
     event.stopPropagation();
 
     // Remove the 'checked' attribute from all radio buttons
-    radioButtons.forEach(button => {
+    radioButtonsAlbum.forEach(button => {
+        button.removeAttribute('checked');
+        console.log( button.removeAttribute('checked', 'checked') , "remove check")
+    });
+
+    this.setAttribute('checked', 'checked');
+
+    console.log(this.setAttribute('checked', 'checked') , "this should now be checked")
+
+    // const event = new Event('change');
+    // this.dispatchEvent(event);
+}
+
+function moveCheckedAttributeApparel(event) {
+    // Identify the clicked radio button
+    const clickedButton = this;
+    console.log(clickedButton, "this what we pressed right")
+
+    // Prevent the default behavior of the radio button
+    // event.preventDefault();
+
+    // Stop the event propagation
+    event.stopPropagation();
+
+    // Remove the 'checked' attribute from all radio buttons
+    radioButtonsApparel.forEach(button => {
         button.removeAttribute('checked');
         console.log( button.removeAttribute('checked', 'checked') , "remove check")
     });
