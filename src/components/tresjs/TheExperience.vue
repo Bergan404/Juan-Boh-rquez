90b93d0022b0a9b6bcc4b0db68a8827393cf1a2b
Bergan Oudshoorn <bergan404@gmail.com>
2023-11-27 07:55:05 -0500

    used tresjs to make a fully 3d 404 page using a model works pretty well

--- src/components/tresjs/TheExperience.vue
+++ src/components/tresjs/TheExperience.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { type ShallowRef, shallowRef } from 'vue';
-import { Stars, OrbitControls } from '@tresjs/cientos';
+import { Stars, OrbitControls, Levioso, MouseParallax } from '@tresjs/cientos';
 import { TresCanvas } from '@tresjs/core';
 import { useRenderLoop } from '@tresjs/core';
 import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three/src/constants.js';
+import Box from "./box.vue";
+import Error from './error.vue';
 
 const gl = {
-    clearColor: "#181c3E",
+    clearColor: "#000000",
     shadows: true,
     alpha: false,
     shadowMapType: BasicShadowMap,
@@ -26,27 +28,30 @@ onLoop(({ delta, elapsed }) => {
     }
 
     if (groupRef.value) {
-        groupRef.value.rotation.y += 0.01
+        groupRef.value.rotation.y += 0.001
     }
 
 })
 
 </script>
+
 <template>
     <div class="tres-container">
-        <TresCanvas>
-            <!-- Camera Postion in the X Y Z -->
-            <TresPerspectiveCamera :position="[0, 0, 5]" :fov="45" :aspect="1" :near="0.1" :far="1000" />
+        <TresCanvas v-bind="gl">
+            <TresPerspectiveCamera :position="[0, 0, 45]" :fov="45" :aspect="1" :near="0.1" :far="1000" />
+            <MouseParallax :factor="5" :ease="3" />
+            <Levioso>
+                <Suspense>
+                    <Error :scale="[0.1, 0.1, 0.1]" :position="0" :rotate-x="0"  />
+                </Suspense>
+            </Levioso>
             <TresGroup ref="groupRef">
-                <TresScene>
-                    <TresMesh ref="boxRef" :scale="1" cast-shadow>
-                        <TresBoxGeometry :args="[1, 1, 1]" />
-                        <TresMeshNormalMaterial />
-                    </TresMesh>
-                </TresScene>
                 <Stars ref="starRef" />
             </TresGroup>
-            <OrbitControls />
+            <!-- <OrbitControls /> -->
+            <TresAmbientLight :intensity="1" />
+            <TresDirectionalLight :position="[-4, -2, 2]" :intensity="1" cast-shadow color="#00f0ff" />
+            <TresDirectionalLight :position="[4, 6, 4]" :intensity="2" cast-shadow color="white" />
         </TresCanvas>
     </div>
 </template>
